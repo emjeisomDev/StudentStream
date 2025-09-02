54140d2dd35aad297370ae2225f2742da2446e76
emjeisomDev <emjeisom.dev@gmail.com>
2025-09-02 20:12:12 -0300

    filterStudentsByAgeGroup method implemented

--- src/student/StudentsStreams.java
+++ src/student/StudentsStreams.java
@@ -1,6 +1,7 @@
 package student;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import student.records.StudentShortListRecord;
 
@@ -16,7 +17,7 @@ public class StudentsStreams {
 					   .toList();
 	}
 	
-	public List<StudentShortListRecord> StudentByBirthDate(Integer month, Integer year){
+	public List<StudentShortListRecord> studentByBirthDate(Integer month, Integer year){
 		return students.stream()
 					     .filter(std -> std.getBirthDate().getMonthValue() == month && std.getBirthDate().getYear() == year)
 					     .map(std -> new StudentShortListRecord(
@@ -24,6 +25,19 @@ public class StudentsStreams {
 									  std.getName(), 
 									  std.getCourse()))
 					     .toList();
+	}
+	
+	public List<StudentShortListRecord> filterStudentsByAgeGroup(Integer lowerLimit, Integer upperLimit){
+		
+		return students.stream()
+				       .collect(Collectors.partitioningBy(
+				    		   stu -> stu.getAge() >= lowerLimit && stu.getAge() <= upperLimit))
+				       .entrySet().stream()
+				       .flatMap(entry -> entry.getValue().stream())
+				       .map(stu -> new StudentShortListRecord(
+				    		   stu.getRegistration(), stu.getName(), stu.getCourse()))
+				       .toList();
+
 	}
 	
 }
